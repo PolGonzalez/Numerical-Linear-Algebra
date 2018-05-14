9e9450f5e289cbb0b5d89694758dfce823a09fe4
PolGonzalez <polgonzalezsierras@gmail.com>
2018-05-14 12:14:41 +0200

    Delete funcions.h

--- funcions.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUNCIONS_H
-#define FUNCIONS_H
- 
-int lu(double **a, int n, int perm[], double tol);
-void resol(double **a, double x[], double b[], int n, int perm[]);
-int inversa(double **a, double **inv_a, int n, double *det_a, double tol);
-
-#endif
